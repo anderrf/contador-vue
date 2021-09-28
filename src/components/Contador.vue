d478abaf672d38a0ba5b8ccd29217c29f010e233
anderrf <anderson_rocha_farias@outlook.com>
2021-09-28 12:36:07 -0300

    Criação do componente Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="contador">
+        <h1>Contador</h1>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'Contador'
+    }
+</script>
+<style scoped>
+
+</style>
