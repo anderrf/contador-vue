15300dfde949541209a6ce5cdad759563da0e9b3
anderrf <anderson_rocha_farias@outlook.com>
2021-09-28 13:42:10 -0300

    Layout

--- src/App.vue
+++ src/App.vue
@@ -16,17 +16,24 @@ export default {
 </script>
 
 <style>
-/*#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}*/
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+  /*#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }*/
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    width: 100vw;
+    min-height: 100vh;
+    font-family: Tahoma;
+    font-size: 18px;
+  }
 </style>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,13 +1,72 @@
 <template>
     <div class="contador">
         <h1>Contador</h1>
+        <div>
+            <p>Valor atual: {{contagem}}</p>
+            <div class="btnDiv">
+                <button type="button" id="btnDec">-</button>
+                <button type="button" id="btnInc">+</button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
     export default{
-        name: 'Contador'
+        name: 'Contador',
+        data(){
+            return{
+                contagem: 0
+            }
+        }
     }
 </script>
 <style scoped>
+    .contador{
+        display: flex;
+        flex-direction: column;
+    }
 
+    .contador h1{
+        text-align: center;
+        margin: 10px auto 50px;
+    }
+
+    .contador > div{
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .btnDiv{
+        width: 80%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin: 20px auto;
+    }
+
+    .btnDiv button{
+        font-size: 1em;
+        font-family: Tahoma;
+        width: 2em;
+        height: 2em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        color: #FFFFFF;
+        font-weight: bolder;
+    }
+
+    #btnDec{
+        background-color: #0000FF;
+    }
+
+    #btnInc{
+        background-color: #00FF00;
+    }
 </style>
